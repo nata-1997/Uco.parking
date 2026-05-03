c66bd0c484d9090b7d8460936b95f1376c6fbd55
nata-1997 <natalia.carmona5843@uco.net.co>
2026-05-03 18:55:42 -0500

    Ajustes de validación de campos

---
+++ src/main/java/co/edu/uco/ucoparking/initializer/features/student/registernewstudent/application/usecase/domain/specification/StudentRegistrationSpecification.java
@@ -0,0 +1,8 @@
+package co.edu.uco.ucoparking.initializer.features.student.registernewstudent.application.usecase.domain.specification;
+
+import co.edu.uco.ucoparking.initializer.features.student.registernewstudent.application.usecase.RegisterNewStudentDomain;
+import jakarta.servlet.Registration;
+
+public interface StudentRegistrationSpecification {
+
+}
