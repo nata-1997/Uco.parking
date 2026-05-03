28fcd5f0de3fdd09f3431226d0001a66a8fbc2d2
nata-1997 <natalia.carmona5843@uco.net.co>
2026-05-03 16:42:07 -0500

    chore: estructura base del proyecto Spring Boot

---
+++ src/main/java/co/edu/uco/UcoParking/UcoParking/UcoParkingApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uco.UcoParking.UcoParking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UcoParkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UcoParkingApplication.class, args);
+	}
+
+}
