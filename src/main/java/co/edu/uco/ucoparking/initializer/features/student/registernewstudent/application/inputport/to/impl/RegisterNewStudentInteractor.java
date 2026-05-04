9142ed42f85091ef9ab6809d2c1b9d1c7fd83c24
Danyx3k <Danyx3k@gmail.com>
2026-05-04 16:25:39 -0500

    Commit creación de Validaciones

--- src/main/java/co/edu/uco/ucoparking/initializer/features/student/registernewstudent/application/inputport/to/impl/RegisterNewStudentInteractor.java
+++ src/main/java/co/edu/uco/ucoparking/initializer/features/student/registernewstudent/application/inputport/to/impl/RegisterNewStudentInteractor.java
@@ -3,6 +3,7 @@ package co.edu.uco.ucoparking.initializer.features.student.registernewstudent.ap
 
 import co.edu.uco.ucoparking.initializer.features.student.registernewstudent.application.inputport.RegisterNewStudentInputPort;
 import co.edu.uco.ucoparking.initializer.features.student.registernewstudent.application.inputport.to.input.RegisterNewStudentInputTO;
+import co.edu.uco.ucoparking.initializer.features.student.registernewstudent.application.inputport.to.mapper.RegisterNewStudentMapper;
 import co.edu.uco.ucoparking.initializer.features.student.registernewstudent.application.usecase.RegisterNewStudentDomain;
 import co.edu.uco.ucoparking.initializer.features.student.registernewstudent.application.usecase.domain.RegisterNewStudentUseCase;
 import org.springframework.stereotype.Service;
@@ -23,8 +24,7 @@ public class RegisterNewStudentInteractor implements RegisterNewStudentInputPort
 
         //Recuerde que el USecas recibe un Domain y el interactor recibe un DTO//
         //MApper o MapStruct//
-        RegisterNewStudentDomain domain = null;
-
+        RegisterNewStudentDomain domain = RegisterNewStudentMapper.toDomain(data);
         return useCase.execute(domain);
     }
 }
--- src/main/java/co/edu/uco/ucoparking/initializer/features/student/registernewstudent/application/usecase/impl/RegisterNewStudentUseCaseImpl.java
+++ src/main/java/co/edu/uco/ucoparking/initializer/features/student/registernewstudent/application/usecase/impl/RegisterNewStudentUseCaseImpl.java
@@ -1,5 +1,6 @@
 package co.edu.uco.ucoparking.initializer.features.student.registernewstudent.application.usecase.impl;
 
+import co.edu.uco.ucoparking.initializer.features.student.registernewstudent.application.outport.RegisterNewStudentOutPort;
 import co.edu.uco.ucoparking.initializer.features.student.registernewstudent.application.usecase.RegisterNewStudentDomain;
 import co.edu.uco.ucoparking.initializer.features.student.registernewstudent.application.usecase.domain.RegisterNewStudentUseCase;
 import org.springframework.stereotype.Service;
@@ -7,8 +8,14 @@ import org.springframework.stereotype.Service;
 @Service //caso de uso//<<--Hace parte de la capa del negocio//
 public class RegisterNewStudentUseCaseImpl implements RegisterNewStudentUseCase {
 
+    private final RegisterNewStudentOutPort outPort;
+
+    public RegisterNewStudentUseCaseImpl(RegisterNewStudentOutPort outPort) {
+        this.outPort = outPort;
+    }
+
     @Override
     public Void execute(RegisterNewStudentDomain data) {
-        return null;
+        return outPort.execute(data);
     }
 }
